e168ac2dae63c4c7f6ef8f5e231038cc29342d91
Эльдар Попов <eldaroid@MacBook-Air-Eldar.local>
2020-01-31 16:07:15 +0300

    expand

--- level3/expand_str.c
+++ level3/expand_str.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   epur_str.c                                         :+:      :+:    :+:   */
+/*   expand_str.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eldaroid <eldaroid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/30 21:06:49 by eldaroid          #+#    #+#             */
-/*   Updated: 2020/01/31 13:53:19 by eldaroid         ###   ########.fr       */
+/*   Created: 2020/01/31 13:54:56 by eldaroid          #+#    #+#             */
+/*   Updated: 2020/01/31 14:24:26 by eldaroid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-
-int		ft_isblank(char c)
+int     ft_isspace(char c)
 {
-	if (c == ' ' || c == '\t')
-		return (1);
-	if (c >= 9 && c <= 13)
-		return (1);
-	return (0);
+    if (c == ' ' || c == '\t')
+        return (1);
+    if (c >= 9 && c <= 13)
+        return (1);
+    return (0);
 }
 
 int     ft_count(char *s1)
@@ -29,46 +28,47 @@ int     ft_count(char *s1)
     int     count;
     int     flag;
 
+    flag = 0;
     i = 0;
     count = 0;
-    flag = 0;
     while (s1[i])
     {
-        if (s1[i] != ' ' && flag == 0)
+        if (!ft_isspace(s1[i]))
         {
-            count++;
             flag = 1;
         }
-        else if (s1[i] == ' ')
+        if (ft_isspace(s1[i]) && flag == 1)
         {
             flag = 0;
+            count++;
         }
         i++;
     }
     return (count);
 }
 
-void    epur_str(char *s1)
+void    expand_str(char *s1)
 {
     int     i;
     int     flag;
-    int     count_words;
+    int     count_word;
 
+    count_word = ft_count(s1) - 1;
+    // printf("count = %d", count_word);
     flag = 0;
     i = 0;
-    count_words = 0;
     while (s1[i])
     {
-        if (s1[i] != ' ' && s1[i] != '\t')
+        if (!ft_isspace(s1[i]))
         {
-            write(1, &s1[i], 1);
             flag = 1;
+            write(1, &s1[i], 1);
         }
-        if (flag == 1 && s1[i] == ' ' && count_words < ft_count(s1) - 1)
+        if (ft_isspace(s1[i]) && flag == 1 && count_word > 0)
         {
-            write(1, " ", 1);
-            count_words++;
+            write(1, "   ", 3);
             flag = 0;
+            count_word--;
         }
         i++;
     }
@@ -78,7 +78,8 @@ int     main(int argc, char **argv)
 {
     if (argc == 2)
     {
-        epur_str(argv[1]);
+        expand_str(argv[1]);
     }
+    write(1, "\n", 1);
     return (0);
 }
